3dcd27706fa46da567b27bbb08747a2d8c583f5d
a8hok <ashokkumar.explore@gmail.com>
2023-04-21 10:20:24 +0530

    updated prettier config

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">
-        NextJS Boilerplate with Typescript
-      </h1>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className='text-3xl font-bold'>NextJS Boilerplate with Typescript</h1>
     </main>
-  )
+  );
 }
